8da3c17e0358880790ef96879361afff62074d04
Homie_Kim <66291768+xXDOPERXx@users.noreply.github.com>
2024-04-14 11:28:13 +0900

    feat: defeated 21943
    idk condition - doubled for

--- answer/W5/21943.cpp
+++ answer/W5/21943.cpp
@@ -1,39 +1,40 @@
 /*
 Strategy :
 backtracking 
+list? drop
+defeated
 
 */
 #include <iostream>
 #include <vector>
+#include <deque>
 using namespace std;
 
-int input[8];  //for input
-bool visited[8] = {false}; //for backtracking 
-int temp[8]; //for backtracking 
 int maxresult = 0;
 
-void Sol(int size, int curval, int P, int M) {
-    //try get value unvisited
-    int index;
-    for(index = 0; index < size; index++) if(!visited[index]) break;
-    
-    //visit finished
-    if(index == size) {
-        maxresult = curval;
+void Sol(vector<int> q, int P) {
+    if(P == 0) {
+        int value = 1;
+        while ( !q.empty() ){
+            value *= q.back();
+            q.pop_back();
+        }
+        maxresult = max(maxresult, value);
         return;
     }
-    //get value & mark visited
-    visited[index] = true;
-    int nextval = input[index];
-    
-    if(M > 0 && P > 0) {
-        Sol(size, curval+nextval, P-1, M);
-        Sol(size, curval*nextval, P, M-1);
-    }
-    else if(M == 0 && P > 0) Sol(size, curval+nextval, P-1, M);
-    else if(P == 0 && M > 0) Sol(size, curval*nextval, P, M-1);
+    else {
+        for(int i = 0; i < q.size()-1; i++) {
+            for(int j = 0; j < q.size()-1-i; j++) {
+                vector<int> newq(q);
+                int newval = newq[i] + newq[i+j+1];
 
-    visited[index] = false;
+                newq.erase(newq.begin()+i);
+                newq.erase(newq.begin()+i+j);
+                newq.insert(newq.begin(), newval);
+                Sol(newq, P-1);
+            }
+        }
+    }
 }
 
 int main(void) {
@@ -41,21 +42,22 @@ int main(void) {
 	cin.tie(NULL);
 	cin.tie(NULL);
 
+    
+    vector<int> input;  //for input
     //operand counts , + counts, * counts, answer
-    int N, P, M;
+    int N, P, M, var;
     cin >> N;
 
     for(int i = 0; i < N; i++) {
-        cin >> input[i];
+        cin >> var;
+        input.push_back(var);
     }
 
     cin >> P >> M;
-    
-    //every start value in input
-    for(int val : input) {
-        Sol(N, val, P, M);
-    }
 
+    //every start value in input
+    Sol(input, P);
+    
     cout << maxresult << endl;
     return 0;
 }
